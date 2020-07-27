c73b0162a66d9de837f85457cb02177cc5b4d53b
Rayhan0Islam0Shagor <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2020-07-27 23:01:36 +0600

    final step

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ generateBtn.addEventListener('click', function () {
 
     document.getElementById('show-pin').value = generateRandomNumber;
 })
+// Submit Section
 document.getElementById('submit').addEventListener('click', function () {
     const showPin = document.getElementById('show-pin').value;
     const numberView = document.getElementById('numView').value;
-    if (showPin === numberView) {
+    if (showPin == numberView) {
         const submitOutput = document.getElementById('matched');
         submitOutput.style.display = 'block';
+        document.getElementById('numView').value = "";
+        document.getElementById('show-pin').value = "";
     }
     if (showPin != numberView) {
-        const submitError = document.getElementById("doesn't match");
+        const submitError = document.getElementById("error");
         submitError.style.display = 'block';
-
+        document.getElementById('numView').value = "";
     }
 })

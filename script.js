a427b2828caac60d6307a440be552f90d76ac25b
Rayhan0Islam0Shagor <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2020-07-28 16:01:46 +0600

    finishing done

--- script.js
+++ script.js
@@ -5,19 +5,37 @@ generateBtn.addEventListener('click', function () {
 
     document.getElementById('show-pin').value = generateRandomNumber;
 })
-// Submit Section
+
+// Submit Button Event Handler
 document.getElementById('submit').addEventListener('click', function () {
     const showPin = document.getElementById('show-pin').value;
     const numberView = document.getElementById('numView').value;
-    if (showPin == numberView) {
+    const warning = document.getElementById('errorCount').value;
+
+    if (showPin == numberView && showPin != 0) {
         const submitOutput = document.getElementById('matched');
         submitOutput.style.display = 'block';
         document.getElementById('numView').value = "";
         document.getElementById('show-pin').value = "";
+        // for remove the 3 try left 
+        const errorCount = document.getElementById('errorCount');
+        errorCount.style.display = 'none';
     }
-    if (showPin != numberView) {
+
+    if (showPin != numberView || showPin == 0) {
         const submitError = document.getElementById("error");
         submitError.style.display = 'block';
         document.getElementById('numView').value = "";
+        document.getElementById('show-pin').value = "";
     }
+})
+
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const removeSuccess = document.getElementById('matched');
+    const removeError = document.getElementById('error');
+    const showTry = document.getElementById('errorCount');
+
+    removeSuccess.style.display = 'none';
+    removeError.style.display = 'none';
+    showTry.style.display = 'block';
 })
